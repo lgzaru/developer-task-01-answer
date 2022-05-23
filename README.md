1acae79959fb48f6069d82064ad658608c0536ff
lgzaru <lgzarue@gmail.com>
2022-05-23 15:46:39 +0200

    Description in README-FILE ITEM 2

--- README.md
+++ README.md
@@ -13,3 +13,14 @@
 
     final SubscriberRequest createdSubscriberRequest = subscriberRequestDao.save(subscriberRequest);
     subscriberRequestDao.save(createdSubscriberRequest);
+
+2. Made changes in EnquiriesServiceImpl class
+   - changed (.persist) and (.update) to save. There is no @PreInsert callback annotation in JPA.
+   - I also changed the fields to final modifier
+   ```sh
+    final SubscriberRequest createdSubscriberRequest = subscriberRequestDao.persist(subscriberRequest);
+    subscriberRequestDao.update(createdSubscriberRequest);
+
+    final SubscriberRequest createdSubscriberRequest = subscriberRequestDao.save(subscriberRequest);
+    subscriberRequestDao.save(createdSubscriberRequest);
+   ```
--- electronic-payments-business/src/main/java/com/econetwireless/epay/business/services/impl/EnquiriesServiceImpl.java
+++ electronic-payments-business/src/main/java/com/econetwireless/epay/business/services/impl/EnquiriesServiceImpl.java
@@ -20,8 +20,8 @@ public class EnquiriesServiceImpl implements EnquiriesService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EnquiriesServiceImpl.class);
 
-    private ChargingPlatform chargingPlatform;
-    private SubscriberRequestDao subscriberRequestDao;
+    private final ChargingPlatform chargingPlatform;
+    private final SubscriberRequestDao subscriberRequestDao;
 
     public EnquiriesServiceImpl(ChargingPlatform chargingPlatform, SubscriberRequestDao subscriberRequestDao) {
         this.chargingPlatform = chargingPlatform;
@@ -33,10 +33,10 @@ public class EnquiriesServiceImpl implements EnquiriesService {
         LOGGER.info("Enquire airtime balance :: Partner Code : {}, Msisdn : {}", partnerCode, msisdn);
         final AirtimeBalanceResponse airtimeBalanceResponse = new AirtimeBalanceResponse();
         final SubscriberRequest subscriberRequest = populate(partnerCode, msisdn);
-        final SubscriberRequest createdSubscriberRequest = subscriberRequestDao.persist(subscriberRequest);
+        final SubscriberRequest createdSubscriberRequest = subscriberRequestDao.save(subscriberRequest);
         final INBalanceResponse inBalanceResponse = chargingPlatform.enquireBalance(partnerCode, msisdn);
         changeSubscriberStateOnBalanceEnquiry(createdSubscriberRequest, inBalanceResponse);
-        subscriberRequestDao.update(createdSubscriberRequest);
+        subscriberRequestDao.save(createdSubscriberRequest);
         airtimeBalanceResponse.setResponseCode(inBalanceResponse.getResponseCode());
         airtimeBalanceResponse.setNarrative(inBalanceResponse.getNarrative());
         airtimeBalanceResponse.setMsisdn(msisdn);
